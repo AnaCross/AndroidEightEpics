1a39f5a445b8e299505b803b99cc2a06004d7a50
Weronika Grela <w.grela.uczelnia@gmail.com>
2017-05-11 19:41:27 +0200

    first commit + first window

---
+++ Licencjat/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace Licencjat
+{
+    [Activity(Label = "Eight Epics", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        Button chooseLevel, instruction, achivement;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            chooseLevel = FindViewById<Button>(Resource.Id.buttonPlay);
+            instruction = FindViewById<Button>(Resource.Id.buttonInstruction);
+            achivement = FindViewById<Button>(Resource.Id.buttonAchievement);
+
+            chooseLevel.Click += (sender, e) => {
+                var intent = new Intent(this, typeof(ChooseLevelActivity));
+                StartActivity(intent);
+            };
+        }
+    }
+}
+
